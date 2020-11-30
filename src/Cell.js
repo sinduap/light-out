2f704f486e7f3164b8db225713e8298733ca5b6c
Sindu Andita <sinduandita@gmail.com>
2020-11-30 21:17:32 +0700

    Light out app react

---
+++ src/Cell.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./Cell.css";
+
+class Cell extends Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.flipCellsAroundMe();
+  }
+
+  render() {
+    let classes = "Cell" + (this.props.isLit ? " Cell-lit" : "");
+    return <td className={classes} onClick={this.handleClick} />;
+  }
+}
+
+export default Cell;
